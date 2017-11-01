3f14a36e98c5a50b5553256e480eafcb183cdb97
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 16:46:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100);
-  var meals = { [item]: 'oatmeal' }
+  var meals = { [item]: '16' }
   cart.push(meals);
   console.log("${item} has been added to your cart.")
   return cart;
