d0a40dbfdf1932faff76f13604dd0bda64c94e8a
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 16:54:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function addToCart(item) {
     [item]: price
   }
   cart.push(newItem);
-  console.log("${item1} has been added to your cart.")
+  console.log(item + " has been added to your cart.")
   return cart;
 }
 
