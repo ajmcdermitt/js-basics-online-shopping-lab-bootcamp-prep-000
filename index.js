d54c1093c206e025ba1fe3580abfd25633bcef13
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 16:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100);
-  var meals = { [item]: 'price' }
+  var meals = { [item]: price }
   //var newItem = {
   //  [item]: price
 //  }
