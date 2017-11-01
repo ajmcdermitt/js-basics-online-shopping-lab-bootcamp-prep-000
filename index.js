c0672c0760c17e68436ab65908399d0851267aa4
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-11-01 17:50:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,11 +73,10 @@ function removeFromCart(item) {
     if (item == itemName){
       cart.splice(i,1);
       return cart;
-    } else if(i == cartLength){
-      console.log("That item is not in your cart.")
-      return cart;
-    }
+    } 
   }
+  console.log("That item is not in your cart.")
+  return cart;
 }
 
 function placeOrder(cardNumber) {
